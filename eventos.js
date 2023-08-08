84abb5abcd262eace1be64efe6bd60a4059286f6
paugarre <paugarre9@gmail.com>
2023-08-08 18:56:11 -0400

    Cambios

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DomContentLoaded", function(){
+    const divButton= document.getElementById ("divButton");
+    divButton.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+      });
+})
